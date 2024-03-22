0b6a42606bfb4c60f8773aec72e30bb7872e6595
Kunjan <choudharykunjan@gmail.com>
2024-03-22 22:28:53 +0530

    basic list created

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,67 @@
-import Image from "next/image";
+"use client";
 import styles from "./page.module.css";
+import Header from "@/components/header/header";
+import { useEffect, useState } from "react";
+
+
 
 export default function Home() {
+  const [userListdata, setUserListdata] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    fetchUserlistData()
+  }, [])
+
+  const fetchUserlistData = async () => {
+    try {
+      setIsLoading(true)
+      const result = await fetch('https://jsonplaceholder.typicode.com/users');
+      const data = await result?.json()
+      if (data) {
+        setUserListdata(data)
+      }
+      console.log(data);
+      setIsLoading(false)
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false)
+    }
+
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <>
+      <Header></Header>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      {
+        isLoading ? <div>Loading...</div> :
+          userListdata.length > 0 &&
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+          userListdata?.map((data, i) => {
+            return (<div key={data.id} className={styles["boxcard-container"]} >
+              <div className={styles["detail-row-container"]}>
+                <div className={styles["card-label"]} >Name : </div>
+                <div className={styles["card-label-value"]} >{data.name}</div>
+              </div>
+              <div className={styles["detail-row-container"]}>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+                <div className={styles["card-label"]}>Email: </div>
+                <div className={styles["card-label-value"]}>{data.email}</div>
+              </div>
+              <div className={styles["detail-row-container"]}>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
+                <div className={styles["card-label"]} >Website: </div>
+                <div className={styles["card-label-value"]} >{data.website}</div>
+              </div>
+            </div>)
+          })
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+
+
+
+      }
+
+    </>
   );
 }
---
+++ src/components/header/header.jsx
@@ -0,0 +1,34 @@
+
+import Link from "next/link";
+import styles from "./header.module.css";
+
+const Header = () => {
+    const headerOptions = [
+        {
+            title: "Home",
+            id: "home",
+            link : "/"
+           
+        },
+        {
+            title: "About",
+            id: "about",
+            link : "/about"
+        },
+
+    ]
+    return (
+        <div className={styles["header-container"]}>
+        {
+        
+            headerOptions.length && headerOptions.map((op )=> 
+                <div className={styles["header-options"]} id={op.id} key={op.id}  ><Link href={op.link}>{op.title}</Link></div>
+
+            )
+
+}
+        </div>
+    )
+}
+
+export default Header
