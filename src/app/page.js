a33540211c22009ea84467cb931a2cfa25820dee
Kunjan <choudharykunjan@gmail.com>
2024-03-23 05:30:41 +0530

    handle responsive design

--- src/app/page.js
+++ src/app/page.js
@@ -22,38 +22,40 @@ export default async function Home() {
     <>
       <Header></Header>
       <ContentWrapper>
-      <SectionTitle sectionText="User List"></SectionTitle>
-      {
-        users?.map((data, i) => {
-          return (
-          
-          <div key={data.id} className={styles["boxcard-container"]} >
-           
-              <Link className={styles["view-detail-text"]} href={`/${data.id}`}>View Detail</Link>
-           
-            <div className={styles["detail-row-container"]}>
-              <div className={styles["card-label"]} >Name : </div>
-              <div className={styles["card-label-value"]} >{data.name}</div>
-            </div>
-            <div className={styles["detail-row-container"]}>
+        <SectionTitle sectionText="User List"></SectionTitle>
+        {
+          users?.map((data, i) => {
+            return (
 
-              <div className={styles["card-label"]}>Email: </div>
-              <div className={styles["card-label-value"]}>{data.email}</div>
-            </div>
-            <div className={styles["detail-row-container"]}>
+              <div key={data.id} className={styles["boxcard-container"]} >
 
-              <div className={styles["card-label"]} >Website: </div>
-              <div className={styles["card-label-value"]} >{data.website}</div>
-            </div>
-          </div>)
-        })
-       
+                <Link className={styles["view-detail-text"]} href={`/${data.id}`}>View Detail</Link>
+
+                <Link  href={`/${data.id}`}>
+                  <div className={styles["detail-row-container"]}>
+                    <div className={styles["card-label"]} >Name: </div>
+                    <div className={styles["card-label-value"]} >{data.name}</div>
+                  </div>
+                </Link>
+                <div className={styles["detail-row-container"]}>
+
+                  <div className={styles["card-label"]}>Email: </div>
+                  <div className={styles["card-label-value"]}>{data.email}</div>
+                </div>
+                <div className={styles["detail-row-container"]}>
+
+                  <div className={styles["card-label"]} >Website: </div>
+                  <div className={styles["card-label-value"]} >{data.website}</div>
+                </div>
+              </div>)
+          })
 
 
 
-      }
 
- </ContentWrapper>
+        }
+
+      </ContentWrapper>
     </>
   );
 }
